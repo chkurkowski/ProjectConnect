cd45220688149abffb6a150d2d90d69475f5fbb4
Chase Kurkowski <chk@fastmail.com>
2021-06-12 12:14:21 -0500

    Initial Commit

---
+++ Assets/Scripts/Platformer/PlatformerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerController : MonoBehaviour
+{
+    public string MoveAxis;
+    public string JumpAxis;
+
+    public float movementSpeed;
+
+    public float jumpForce;
+
+    private bool canJump = true;
+
+
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxisRaw(MoveAxis) * movementSpeed * Time.deltaTime, 0f, 0f);
+
+        if (Input.GetAxisRaw(JumpAxis) == 1.0f && canJump)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            canJump = false;
+        }
+
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            canJump = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && Input.GetKey(KeyCode.Space))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Platformer/SuperPlatformerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperPlatformerController : MonoBehaviour
+{
+    public float moveSpeed = 20f;
+    public float fireRate = 10f;
+    private float currentTimer = 0f;
+    private bool canJump = true;
+
+    public float jumpForce;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            //break back into 2 parts
+        }
+
+        if (Input.GetAxis("Fire1") != 0 || Input.GetAxis("Fire2") != 0)
+        {
+            if (currentTimer >= 100f)
+            {
+                currentTimer = 0f;
+                Fire();
+            }
+
+        }
+
+        currentTimer += Time.deltaTime * fireRate;
+
+        if (Input.GetAxisRaw("Vertical") == 1.0f && canJump)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            canJump = false;
+        }
+
+
+    }
+
+    void Fire()
+    {
+        GameObject newBullet = ObjectPooler.instance.SpawnFromPool("Bullet", transform.position, Quaternion.identity);
+
+        Bullet newBulletScript = newBullet.GetComponent<Bullet>();
+
+        newBulletScript.setSpeed(new Vector2(Input.GetAxisRaw("Fire1"), Input.GetAxisRaw("Fire2")));
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Death" || collision.gameObject.tag == "Enemy")
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (collision.gameObject.tag == "Floor")
+        {
+            canJump = true;
+        }
+    }
+}
